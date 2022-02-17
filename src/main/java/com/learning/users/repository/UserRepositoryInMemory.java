ad6d0cb0e47301595d04a77e889e7f4126b9dbc2
Robinson Carvalho <robinsonpc@hotmail.com>
2022-02-17 18:27:40 +0000

    update feature implemented.

--- src/main/java/com/learning/users/repository/UserRepositoryInMemory.java
+++ src/main/java/com/learning/users/repository/UserRepositoryInMemory.java
@@ -5,6 +5,7 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import javax.management.openmbean.InvalidKeyException;
 import javax.management.openmbean.KeyAlreadyExistsException;
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class UserRepositoryInMemory implements UserRepository{
@@ -21,14 +22,21 @@ public class UserRepositoryInMemory implements UserRepository{
         int idUser = mapStorage.size() + 1;
         user.setId(idUser);
         mapStorage.put(user.getEmail(), user);
+        System.out.println("User created successfully.");
     }
 
     public void update(User user){
+
         Set<ConstraintViolation<User>> violations = VALIDATOR.validate(user);
         if(!violations.isEmpty()){
             throw new ConstraintViolationException(violations);
         }
+
+        Map<String, LocalDateTime> StorageUserUpdated = new HashMap<>();
         mapStorage.replace(user.getEmail(), user);
+        StorageUserUpdated.replace(user.getEmail(), LocalDateTime.now());
+        System.out.println("User data updated successfully.");
+
     }
 
     public User read(String email){
