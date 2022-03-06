67e1323029a29be7ac767faa9cf149130b6af0f5
Robinson Carvalho <robinsonpc@hotmail.com>
2022-03-06 23:27:57 +0000

    unused import removed from project

--- src/main/java/com/learning/users/repository/UserRepository.java
+++ src/main/java/com/learning/users/repository/UserRepository.java
@@ -1,12 +1,8 @@
 package com.learning.users.repository;
 
 import com.learning.users.model.User;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validation;
 import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator;
-
-import java.util.Set;
 
 public interface UserRepository {
 
--- src/main/java/com/learning/users/repository/UserRepositoryInMemory.java
+++ src/main/java/com/learning/users/repository/UserRepositoryInMemory.java
@@ -3,7 +3,6 @@ package com.learning.users.repository;
 import com.learning.users.model.User;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
-
 import javax.management.openmbean.KeyAlreadyExistsException;
 import java.util.*;
 
@@ -11,6 +10,7 @@ public class UserRepositoryInMemory implements UserRepository{
 
     Map<String, User> mapStorage = new HashMap<>();
 
+    @Override
     public void create(User user){
         Set<ConstraintViolation<User>> violations = VALIDATOR.validate(user);
         if(!violations.isEmpty()){
@@ -21,10 +21,11 @@ public class UserRepositoryInMemory implements UserRepository{
         int idUser = mapStorage.size() + 1;
         user.setId(idUser);
         mapStorage.put(user.getEmail(), user);
+        System.out.println("User created successfully.");
     }
 
+    @Override
     public int count(){
         return mapStorage.size();
     }
-
 }
